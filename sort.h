05544724b788ad7d3b8ad24c5a69995eb09bfe3b
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-23 09:51:55 +0300

    sort.h

---
+++ sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdbool.h>
+#include <stddef.h>
+void swap(int* xp, int* yp);
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+#include <stdio.h>
+#include <stdlib.h>
+#endif
