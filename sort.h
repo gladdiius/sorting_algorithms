bfe3e1c8922c6cfecebc019bbbdde100eaccd6f2
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-23 10:12:36 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #define SORT_H
 #include <stdbool.h>
 #include <stddef.h>
-void swap(int* xp, int* yp);
+void swap(int *xp, int *yp);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 #include <stdio.h>
