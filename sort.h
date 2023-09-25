6186497397a1c8db4a97c5f25ca49c7805e018b2
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-25 15:12:30 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -16,6 +16,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 void print_list(const listint_t *list);
